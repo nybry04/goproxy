7eff528478924b9356447764c1cd0f854344fb97
nybry04 <alexandernpe@gmail.com>
2024-12-30 17:37:53 +0700

    0.0.8

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/nybry04/goproxy/v2
 
 go 1.23.1
+
+require (
+	github.com/BRA1L0R/go-mcproto v0.3.1 // indirect
+	github.com/Tnze/go-mc v1.16.5 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/supabase-community/functions-go v0.0.0-20220927045802-22373e6cb51d // indirect
+	github.com/supabase-community/gotrue-go v1.2.0 // indirect
+	github.com/supabase-community/postgrest-go v0.0.11 // indirect
+	github.com/supabase-community/storage-go v0.7.0 // indirect
+	github.com/supabase-community/supabase-go v0.0.4 // indirect
+	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,63 @@
 package main
 
 import (
+	"fmt"
+	"github.com/buger/jsonparser"
+	"github.com/supabase-community/supabase-go"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
+type User struct {
+	Id       string
+	Ip       string
+	Username string
+	Password string
+}
+
+var users map[string]User
+var usersMutex sync.Mutex
+
+func fetchUsers(url, token string) {
+	client, err := supabase.NewClient(url, token, &supabase.ClientOptions{})
+	if err != nil {
+		fmt.Println("Cannot initialize client", err)
+	}
+	data, _, _ := client.From("minecraft").Select("*", "exact", false).Execute()
+
+	localUsers := make(map[string]User)
+
+	jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		id, _ := jsonparser.GetString(value, "id")
+		ip, _ := jsonparser.GetString(value, "ip")
+		username, _ := jsonparser.GetString(value, "username")
+		password, _ := jsonparser.GetString(value, "password")
+		localUsers[ip] = User{id, ip, username, password}
+	})
+	usersMutex.Lock()
+	users = localUsers
+	usersMutex.Unlock()
+}
+
 func main() {
+	log.Printf("Version 0.0.8")
 	listen := os.Getenv("LISTEN")
 	target := os.Getenv("TARGET")
+	limbo := os.Getenv("LIMBO")
+	supabase_url := os.Getenv("SUPABASE_URL")
+	supabase_token := os.Getenv("SUPABASE_TOKEN")
+
+	go func() {
+		for {
+			fetchUsers(supabase_url, supabase_token)
+			time.Sleep(1 * time.Minute)
+		}
+	}()
 
 	listener, err := net.Listen("tcp", listen)
 	if err != nil {
@@ -25,20 +73,38 @@ func main() {
 			log.Printf("Failed to accept connection: %s\n", err)
 			continue
 		}
-		go handleConnection(clientConn, target)
+		log.Printf("New connection from %s\n", clientConn.RemoteAddr().String())
+		go handleConnection(clientConn, target, limbo)
 	}
 }
 
-func handleConnection(clientConn net.Conn, targetAddr string) {
+func handleConnection(clientConn net.Conn, targetAddr string, limboAddr string) {
 	defer clientConn.Close()
 
-	targetConn, err := net.Dial("tcp", targetAddr)
+	ipUser := users[strings.Split(clientConn.RemoteAddr().String(), ":")[0]]
+
+	var target string
+
+	if ipUser.Ip == "" {
+		target = limboAddr
+	} else {
+		target = targetAddr
+	}
+
+	targetConn, err := net.Dial("tcp", target)
 	if err != nil {
 		log.Printf("Failed to connect to target: %s\n", err)
 		return
 	}
 	defer targetConn.Close()
 
-	go io.Copy(targetConn, clientConn)
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		io.Copy(targetConn, clientConn)
+	}()
 	io.Copy(clientConn, targetConn)
+
+	<-done
 }
